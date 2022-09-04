1043d7bbea1390580293800916c928e7215b5270
doubleau15 <93496495+doubleau15@users.noreply.github.com>
2022-09-04 18:44:02 -0500

    Update set-theme.sh

--- scripts/set-theme.sh
+++ scripts/set-theme.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
 # Mensaje
-echo "Set theme"
-echo "1) CalmSky"
-echo "2) CalmSky-Dark"
-echo "3) Cancelar"
+echo "┌──────────────────┐"
+echo "│ Set theme        │"
+echo "│ 1) CalmSky       │"
+echo "│ 2) CalmSky-Dark  │"
+echo "│ 3) Cancel        │"
+echo "└──────────────────┘"
 
 # Leer opción
 read opciont
@@ -12,16 +14,36 @@ case $opciont in
 
  # Tema claro
  1)
+ clear
+ echo "┌──────────────┐"
+ echo "│ Applying...  │"
+ echo "└──────────────┘"
  cp $HOME/.jwm/CalmSky/Light/jwmrc $HOME/.jwmrc
+ clear
+ echo "┌───────┐"
+ echo "│ Done  │"
+ echo "└───────┘"
  ;;
 
  # Tema Oscuro
  2)
+ clear
+ echo "┌──────────────┐"
+ echo "│ Applying...  │"
+ echo "└──────────────┘"
  cp $HOME/.jwm/CalmSky/Dark/jwmrc $HOME/.jwmrc
+ clear
+ echo "┌───────┐"
+ echo "│ Done  │"
+ echo "└───────┘"
  ;;
 
  #Cancelar
  3)
+ clear
+ echo "┌────────────┐"
+ echo "│ Cancelled  │"
+ echo "└────────────┘"
  exit 1
  ;;
 esac
